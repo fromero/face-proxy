c351d4a51bb8154e0e7f8d0c52396f21eae6b227
fromero <francisco.romero.orcajada@gmail.com>
2021-04-05 13:10:12 +0200

    ps

---
+++ server.ts
@@ -0,0 +1,18 @@
+async function handleRequest(request) {
+    const response = await fetch("https://https://face.gob.es/api/v2/relaciones?cif=P0200300B", {
+    headers: {
+      accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    const res = await response.json();
+    return new Response(JSON.stringify(res), {
+      headers: {
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin":  "*",
+        "Access-Control-Allow-Methods": "*",
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    });
+  }
+}
