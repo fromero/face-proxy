a7bb99f81a31262863f043bb420efb4a7bbda4ae
fromero <francisco.romero.orcajada@gmail.com>
2021-04-05 13:20:32 +0200

    pd

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
-async function handleRequest(request: any) {
+import {ServerRequest} from 'https://deno.land/std@0.53.0/http/server.ts'
+async function handleRequest(request: ServerRequest) {
     const response = await fetch("https://https://face.gob.es/api/v2/relaciones?cif=P0200300B", {
     headers: {
       accept: "application/json",
